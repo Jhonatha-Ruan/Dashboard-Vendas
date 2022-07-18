40a39c7aad8769f5dde3e0051970afa4e7205868
Jhonatha Ruan <jhonatanruan95@gmail.com>
2022-07-18 01:03:53 -0300

    first commit

---
+++ src/app/pages/charts/chartjs/chartjs-line.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnDestroy } from '@angular/core';
+import { NbThemeService, NbColorHelper } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-chartjs-line',
+  template: `
+    <chart type="line" [data]="data" [options]="options"></chart>
+  `,
+})
+export class ChartjsLineComponent implements OnDestroy {
+  data: any;
+  options: any;
+  themeSubscription: any;
+
+  constructor(private theme: NbThemeService) {
+    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+
+      const colors: any = config.variables;
+      const chartjs: any = config.variables.chartjs;
+
+      this.data = {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [{
+          data: [65, 59, 80, 81, 56, 55, 40, 23, 45, 12, 56, 67],
+          label: 'Vendas',
+          backgroundColor: NbColorHelper.hexToRgbA(colors.primary, 0.3),
+          borderColor: colors.primary,
+        }, {
+          data: [28, 48, 40, 19, 86, 27, 90, 12, 34, 54, 56, 32,],
+          label: 'Clientes',
+          backgroundColor: NbColorHelper.hexToRgbA(colors.danger, 0.3),
+          borderColor: colors.danger,
+        },
+        ],
+      };
+
+      this.options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: true,
+                color: chartjs.axisLineColor,
+              },
+              ticks: {
+                fontColor: chartjs.textColor,
+              },
+            },
+          ],
+        },
+        legend: {
+          labels: {
+            fontColor: chartjs.textColor,
+          },
+        },
+      };
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
+  }
+}
---
+++ src/app/pages/charts/grafico/grafico.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-grafico',
+  templateUrl: './grafico.component.html',
+  styleUrls: ['./grafico.component.scss']
+})
+export class GraficoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
